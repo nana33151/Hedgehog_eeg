c0c50f344b0990e15460f13906adefd41592946f
nana33151 <164616449+nana33151@users.noreply.github.com>
2024-03-23 20:21:04 +0300

    remove blyat >:/

--- eeg_model/import torch.py
+++ eeg_model/import torch.py
@@ -1,7 +1,2 @@
-import torch
-seq_len = 20
-tgt_vocab_size = 5
-class_matrices = torch.zeros(seq_len, tgt_vocab_size-1)
-last_class = torch.ones(seq_len, 1)
-labels_matrices = torch.cat([class_matrices, last_class],1)
-print(labels_matrices.shape)
+import os
+os.remove("model.onnx")
--- eeg_model/main.py
+++ eeg_model/main.py
@@ -2,8 +2,7 @@ import torch
 from torch import nn
 import math
 import mne
-from mne import Epochs
-from mne import io
+import os
 from mne.datasets import eegbci
 from mne import channels
 import numpy as np
@@ -32,14 +31,14 @@ FF_DIM = 64
 DROPOUT = 0.5
 N_CHANNELS = 22
 SEQ_LEN = 1000
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
 class PositionalEncoding(nn.Module):
     def __init__(self, seq_length, model_dim):
         super(PositionalEncoding,self).__init__()
         self.sl = seq_length
         self.md = model_dim
-        self.encodings_matrices = torch.zeros(self.sl, self.md).cuda()
-        position = torch.arange(0,self.sl,dtype = torch.float).unsqueeze(1).cuda()
+        self.encodings_matrices = torch.zeros(self.sl, self.md).to(device)
+        position = torch.arange(0,self.sl,dtype = torch.float).unsqueeze(1).to(device)
         for p in range(self.sl):
           for i in range(self.md):
             t = p/(10000**(2*i/self.md))
@@ -49,7 +48,7 @@ class PositionalEncoding(nn.Module):
                 self.encodings_matrices[p,i] = math.sin(t)
 
     def forward(self,x):
-      output = torch.cat((x,self.encodings_matrices),1).cuda()
+      output = torch.cat((x,self.encodings_matrices),1).to(device)
       return output
 
 class Transformer(nn.Module):
@@ -72,19 +71,19 @@ class Transformer(nn.Module):
         self.optimizer = optim.AdamW(self.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
         self.softmax = nn.Softmax(dim = 1)
         self.mask = torch.triu(torch.ones(seq_lenght,d_model*2), diagonal=1)
-        self.mask = self.mask.int().float().cuda()
+        self.mask = self.mask.int().float().to(device)
 
     def forward(self,src,tgt, valid = False):
         if valid:
-            tgt_embedded = tgt.cuda()
+            tgt_embedded = tgt.to(device)
         else:
-            tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt.transpose(0,1)))).cuda()
+            tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt.transpose(0,1)))).to(device)
             tgt_embedded = tgt_embedded * self.mask
-        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src.transpose(0,1)))).cuda()
-        enc_output = self.encoder.forward(src_embedded).cuda()
-        dec_output = self.decoder.forward(tgt_embedded, enc_output).cuda()
-        fc_output = self.fc(dec_output).cuda()
-        output = self.softmax(fc_output).cuda()
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src.transpose(0,1)))).to(device)
+        enc_output = self.encoder.forward(src_embedded).to(device)
+        dec_output = self.decoder.forward(tgt_embedded, enc_output).to(device)
+        fc_output = self.fc(dec_output).to(device)
+        output = self.softmax(fc_output).to(device)
         return [output, src_embedded]
 
     def accuracy(self, output, labels):
@@ -118,14 +117,14 @@ class Transformer(nn.Module):
 
 
 def labels_to_matrices(labels, tgt_vocab_size, seq_len):
-    class_matrices = torch.zeros(seq_len, tgt_vocab_size-1).cuda()
-    last_class = torch.ones(seq_len, 1).cuda()
-    labels_matrices = torch.cat([class_matrices, last_class],1).cuda()
+    class_matrices = torch.zeros(seq_len, tgt_vocab_size-1).to(device)
+    last_class = torch.ones(seq_len, 1).to(device)
+    labels_matrices = torch.cat([class_matrices, last_class],1).to(device)
     coordinates = list(labels.keys())
     for i in coordinates:
         labels_matrices[i][labels[i]-1] = 1.0
         labels_matrices[i][tgt_vocab_size-1] = 0.0
-    labels_matrices = labels_matrices.type(torch.FloatTensor).cuda()
+    labels_matrices = labels_matrices.type(torch.FloatTensor).to(device)
     return(labels_matrices)
 
 def slice_to_batches(raw_data, batch_size, n_batches, n_chans):
@@ -134,9 +133,9 @@ def slice_to_batches(raw_data, batch_size, n_batches, n_chans):
     single_batch = []
     for i in range(n_chans):
       element = raw_data[i][(b*batch_size):((b+1)*batch_size)]
-      element = element.unsqueeze(0).cuda()
+      element = element.unsqueeze(0).to(device)
       single_batch.append(element)
-    tensored = torch.cat(single_batch,0).type(torch.FloatTensor).cuda()
+    tensored = torch.cat(single_batch,0).type(torch.FloatTensor).to(device)
     batch_list.append(tensored)
   return batch_list
 
@@ -183,11 +182,10 @@ pred_batches = []
 
 for i in range(len(validating_set)):
     valid_raw = validating_set[i].raw
-    raw_data = torch.from_numpy(valid_raw.get_data()).cuda()
-    print(raw_data.shape)
+    raw_data = torch.from_numpy(valid_raw.get_data()).to(device)
     n_batches = raw_data.size(1)//SEQ_LEN
     validating_datasets += slice_to_batches(raw_data, SEQ_LEN, n_batches, N_CHANNELS)
-    true_preds = torch.from_numpy(mne.events_from_annotations(valid_raw)[0]).cuda()
+    true_preds = torch.from_numpy(mne.events_from_annotations(valid_raw)[0]).to(device)
     pred_dict = {}
     for l in true_preds:
         pred_dict[l[0].item()] = l[2].item()
@@ -196,32 +194,30 @@ for i in range(len(validating_set)):
 
 for i in range(len(training_datasets)):
     train_raw = training_set[i].raw
-    raw_data = torch.from_numpy(train_raw.get_data()).cuda()
+    raw_data = torch.from_numpy(train_raw.get_data()).to(device)
     n_batches = raw_data.size(1)//SEQ_LEN
     training_datasets += slice_to_batches(raw_data, SEQ_LEN, n_batches, N_CHANNELS)
     test_raw = target_set[i].raw
-    traw_data = torch.from_numpy(test_raw.get_data()).cuda()
-    labels = torch.from_numpy(mne.events_from_annotations(test_raw)[0]).cuda()
+    traw_data = torch.from_numpy(test_raw.get_data()).to(device)
+    labels = torch.from_numpy(mne.events_from_annotations(test_raw)[0]).to(device)
     labels_dict = {}
     for l in labels:
         labels_dict[l[0].item()] = l[2].item()
     labels_matrices = labels_to_matrices(labels_dict, TGT_VOCAB_SIZE, n_batches * SEQ_LEN)
     labels_batches += torch.split(labels_matrices, SEQ_LEN)
-    
     test_datasets += slice_to_batches(traw_data, SEQ_LEN, n_batches, N_CHANNELS)
 
 
 transformer = Transformer(DIM,NUM_HEADS,NUM_LAYERS,FF_DIM,SEQ_LEN,DROPOUT,N_CHANNELS,TGT_VOCAB_SIZE)#d_model, num_heads, num_layers, d_ff, seq_lenght, dropout,in_d,tgt_vocab_size
-transformer = transformer.cuda()
-torch.save(transformer, "model.onnx")
-
+transformer = transformer.to(device)
+#torch.save(transformer, "model.onnx")
 running_loss = 0
 last_loss = 0
 running_corr = 0
 EPOCHS = 1
 output = 0
 start_time = time.time()
-best_loss = float('inf')
+best_loss = 9999999999999999.9
 for j in range(EPOCHS):
     for i in range(len(training_datasets)):
         transformer.train()
@@ -230,11 +226,11 @@ for j in range(EPOCHS):
         running_corr += corr
         if loss < best_loss:
             best_loss = loss
+            os.remove("model.onnx")
             torch.save(transformer, "model.onnx")
         if i % 10 == 9:
             last_loss = running_loss / 10 
-            
-            print('  batch {} loss: {} correct {}'.format(i + 1, last_loss, running_corr/10000))
+            print(f"batch {i+1} loss: {last_loss} correct {running_corr/10000}")
             running_loss = 0
             running_corr = 0
 
@@ -249,6 +245,6 @@ for i in range(len(validating_datasets)):
     valid_corr += corr
     if i % 10 == 9:
         last_loss = running_loss / 10 
-        print('  batch {} loss: {} correct {}'.format(i + 1, last_loss, running_corr/10000))
+        print(f"batch {i+1} loss: {valid_loss} correct {valid_corr/10000}")
         valid_loss = 0
         valid_corr = 0
