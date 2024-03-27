eb2129be4848027d4d3f5c9e86474a34e1ddede6
nana33151 <164616449+nana33151@users.noreply.github.com>
2024-03-27 14:16:28 +0300

    Add files via upload

--- eeg_model/main.py
+++ eeg_model/main.py
@@ -41,7 +41,8 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(0,self.sl,dtype = torch.float).unsqueeze(1).to(device)
         for p in range(self.sl):
           for i in range(self.md):
-            t = p/(10000**(2*i/self.md))
+            d = 2*seq_length/math.pi
+            t = p/(d**(2*i/self.md))
             if i%2 == 0:
                 self.encodings_matrices[p,i] = math.cos(t)
             else:
@@ -52,7 +53,7 @@ class PositionalEncoding(nn.Module):
       return output
 
 class Transformer(nn.Module):
-    def __init__(self, d_model, num_heads, num_layers, d_ff, seq_lenght, dropout,in_d,tgt_vocab_size):
+    def __init__(self, d_model, num_heads, num_layers, d_ff, seq_lenght,in_d,tgt_vocab_size):
         super(Transformer, self).__init__()
 
         self.in_d = in_d
@@ -60,13 +61,12 @@ class Transformer(nn.Module):
         self.encoder_embedding = nn.Linear(in_d,d_model)#!
         self.decoder_embedding = nn.Linear(tgt_vocab_size,d_model)#!
         self.positional_encoding = PositionalEncoding(seq_lenght, d_model)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model*2, num_heads, d_ff, dropout)
-        self.decoder_layer = nn.TransformerDecoderLayer(d_model*2, num_heads, d_ff, dropout)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model*2, num_heads, d_ff, dropout = 0., activation= "gelu")
+        self.decoder_layer = nn.TransformerDecoderLayer(d_model*2, num_heads, d_ff, dropout = 0., activation= "gelu")
 
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers)
         self.decoder = nn.TransformerDecoder(self.decoder_layer, num_layers)
         self.fc = nn.Linear(d_model*2, tgt_vocab_size)
-        self.dropout = nn.Dropout(dropout)
         self.loss = nn.CrossEntropyLoss()
         self.optimizer = optim.AdamW(self.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
         self.softmax = nn.Softmax(dim = 1)
@@ -77,17 +77,18 @@ class Transformer(nn.Module):
         if valid:
             tgt_embedded = tgt.to(device)
         else:
-            tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt))).to(device)
+            tgt_embedded = self.positional_encoding(self.decoder_embedding(tgt)).to(device)
             tgt_embedded = tgt_embedded * self.mask
         
-        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src.transpose(0,1)))).to(device)
+        src_embedded = self.positional_encoding(self.encoder_embedding(src.transpose(0,1))).to(device)
         enc_output = self.encoder.forward(src_embedded).to(device)
         dec_output = self.decoder.forward(tgt_embedded, enc_output).to(device)
-        fc_output = self.fc(dec_output).to(device)
+        activated_dec = nn.functional.gelu(dec_output).to(device)
+        fc_output = self.fc(activated_dec).to(device)
         output = self.softmax(fc_output).to(device)
         return [output, src_embedded]
 
-    def weightedAccuracy(self, output, labels,gistogram):
+    def weightedAccuracy(self, output, labels):
         correct = 0
         all_points = output.size(0)
         pred = torch.argmax(output,dim = 1) #[tensor of indices]
@@ -99,23 +100,26 @@ class Transformer(nn.Module):
             sum += 1/gistogram[true_pred[i]]    
         return correct/sum
 
-    def training_step(self, batch,gistogram):
+    def training_step(self, batch):
         data, lab = batch
         self.optimizer.zero_grad()
         output, _ = self.forward(data, lab)
         f_output = output.view(-1, TGT_VOCAB_SIZE)
-        loss = self.loss(f_output,lab)
-        loss.backward()
+        gistogram_tensor = torch.tensor(gistogram)
+        weighted_output = torch.div(f_output, gistogram_tensor).to(device)
+        weighted_labels = torch.div(lab, gistogram_tensor).to(device)
+        loss = self.loss(weighted_output, weighted_labels)
+        loss.backward(loss)
         self.optimizer.step()
-        accuracy = self.weightedAccuracy(f_output,lab,gistogram)
+        accuracy = self.weightedAccuracy(f_output,lab)
         return [loss, accuracy]
 
-    def valid_step(self,batch,gistogram):
+    def valid_step(self,batch):
         data,y,true_pred = batch
         output, embeddings = self.forward(data, y, valid = True)
         f_output = output.view(-1,TGT_VOCAB_SIZE)
         loss = self.loss(f_output, true_pred)
-        accuracy = self.weightedAccuracy(f_output, true_pred,gistogram)
+        accuracy = self.weightedAccuracy(f_output, true_pred)
         return [loss, accuracy,embeddings]
 
 
@@ -215,12 +219,12 @@ for i in range(len(training_set)):
 
 print(gistogram)
 
-transformer = Transformer(DIM,NUM_HEADS,NUM_LAYERS,FF_DIM,SEQ_LEN,DROPOUT,N_CHANNELS,TGT_VOCAB_SIZE)#d_model, num_heads, num_layers, d_ff, seq_lenght, dropout,in_d,tgt_vocab_size
+transformer = Transformer(DIM,NUM_HEADS,NUM_LAYERS,FF_DIM,SEQ_LEN,N_CHANNELS,TGT_VOCAB_SIZE)#d_model, num_heads, num_layers, d_ff, seq_lenght, dropout,in_d,tgt_vocab_size
 transformer = transformer.to(device)
 torch.save(transformer, "model.onnx")
 running_loss = 0
 last_loss = 0
-running_corr = 0
+running_acc = 0
 EPOCHS = 1
 output = 0
 start_time = time.time()
@@ -230,9 +234,9 @@ for j in range(EPOCHS):
     running_corr = 0
     for i in range(len(training_datasets)):
         transformer.train()
-        loss, corr = transformer.training_step([training_datasets[i],labels_batches[i]], gistogram)
+        loss, acc = transformer.training_step([training_datasets[i],labels_batches[i]])
         running_loss += loss.item()
-        running_corr += corr
+        running_acc += acc
         if loss < best_loss:
             best_loss = loss
             os.remove("model.onnx")
@@ -240,25 +244,25 @@ for j in range(EPOCHS):
         if i % 10 == 9:
             last_loss = running_loss / 10 
             print("training step")
-            print(f"batch {i+1} loss: {last_loss} correct {running_corr/10}")
+            print(f"batch {i+1} median loss: {last_loss}, meidan accuracy: {running_acc/10}")
             running_loss = 0
-            running_corr = 0
+            running_acc = 0
 
 embeddings = torch.randn(SEQ_LEN,DIM*2)
 valid_loss = 0
 last_loss = 0
-valid_corr = 0
+valid_acc = 0
 
 for i in range(len(validating_datasets)):
-    loss,corr,embeddings = transformer.valid_step([validating_datasets[i],embeddings, pred_batches[i]],gistogram)
+    loss,acc,embeddings = transformer.valid_step([validating_datasets[i],embeddings, pred_batches[i]])
     valid_loss += loss.item()
-    valid_corr += corr
+    valid_acc += acc
     if i % 10 == 9:
         last_loss = running_loss / 10 
         print("validating step")
-        print(f"batch {i+1} loss: {valid_loss} correct {valid_corr/10}")
+        print(f"batch {i+1} median loss: {valid_loss}, median accuracy: {valid_acc/10}")
         valid_loss = 0
-        valid_corr = 0
+        valid_acc = 0
 """
 list gistogram = {0, 0, 0, 0, 0}
 
